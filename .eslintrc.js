b88733ea91bb5c9b59e105f72417c9b43a58b187
unknown <dominik.obuch@wp.pl>
2023-06-10 21:31:11 +0200

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
---
+++ dev/scripts/app.js
@@ -0,0 +1,11 @@
+// let stepsEl = document.getElementsByClassName("step");
+// for (let i = 1; i < stepsEl.length; i++) {
+//     console.log(stepsEl[i])
+//     stepsEl[i].dataset.x = window.innerWidth*i+1 
+// }
+// const FULLSIZESLIDES = [1,2]
+// let stepsEl = document.getElementsByClassName("step");
+// for (let i = 0; i < FULLSIZESLIDES.length; i++) {
+//     console.log(stepsEl[FULLSIZESLIDES[i]])
+//     stepsEl[FULLSIZESLIDES[i]].dataset.x = window.innerWidth*i+1 
+// }
